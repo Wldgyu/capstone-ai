0054e77ac82406550a114866b91854aae4918349
Wldgyu <162098210+Wldgyu@users.noreply.github.com>
2026-06-13 17:44:33 +0900

    Update version_2_0.py

--- version_2_0.py
+++ version_2_0.py
@@ -305,4 +305,4 @@ def get_recommendations(request: RecommendationRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
